2c82fae158abf44ec0b92646aa44a4cfad59e2f2
KojiroSchatten <kouschatten@gmail.com>
2024-06-08 01:07:06 +0900

    feat: line whisper what you said

--- index.ts
+++ index.ts
@@ -1,32 +1,42 @@
-import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+import { Hono } from 'hono'
 
-const handler = async (request: Request): Promise<Response> => {
-  const requestUrl = new URL(request.url);
-  const pokemonNumber = requestUrl.searchParams.get("number");
+// アプリケーションの作成
+const app = new Hono()
 
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`;
-  const resp = await fetch(url, {
-    headers: {
-      accept: "application/json",
-    },
-  });
+app.post('/webhook', async c => {
+  const data = await c.req.json()
+  const replies: Promise<Response>[] = []
 
-  let body = `<p>Not Found. number: ${pokemonNumber}`;
-  if (resp.status === 200) {
-    const json = await resp.json();
-    body = `
-      <p>Id: ${json["id"]}</p>
-      <p>Name: ${json["name"]}</p>
-      <img src="${json["sprites"]["front_default"]}" alt="${json["name"]}">
-    `;
+  for (const event of data.events) {
+    // イベントでループ
+    if (event.type !== 'message') return // メッセージでないイベントは無視
+
+    const { message, replyToken } = event
+
+    if (message.type !== 'text') return // テキストメッセージでないイベントは無視
+
+    const textMessage: string = message.text // ユーザーの発言を取得
+
+    const replyData = {
+      replyToken,
+      messages: [{
+        type: "text",
+        text: `あなたはさっき、${textMessage}と言った！`
+      }],
+    } // リプライするデータを作成
+    replies.push(fetch("https://api.line.me/v2/bot/message/reply", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + Deno.env.get("LINE_TOKEN"),
+      },
+      "body": JSON.stringify(replyData),
+    })) // リプライ
   }
+  await Promise.all(replies)
 
-  return new Response(body, {
-    status: resp.status,
-    headers: {
-      "content-type": "text/html",
-    },
-  });
-};
+  return c.text('OK')
+})
 
-serve(handler);
+// サーバーを起動
+Deno.serve(app.fetch)
