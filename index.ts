b0e72beaf01cc1dbe25391fc77b3ab50048047e5
KojiroSchatten <kouschatten@gmail.com>
2024-06-08 14:01:13 +0900

    LINE Messageing APIのローカル開発セットアップ

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 import { Hono } from 'hono'
+import { load } from "https://deno.land/std@0.203.0/dotenv/mod.ts"
 
 // アプリケーションの作成
 const app = new Hono()
 
 app.post('/webhook', async c => {
+  await load({ export: true })
   const data = await c.req.json()
   const replies: Promise<Response>[] = []
 
   for (const event of data.events) {
+    console.log('data', data)
     // イベントでループ
-    if (event.type !== 'message') return // メッセージでないイベントは無視
+    if (event.type !== 'message') {
+      console.warn('data', data)
+      return
+    }
 
     const { message, replyToken } = event
 
@@ -24,6 +30,7 @@ app.post('/webhook', async c => {
         text: `あなたはさっき、${textMessage}と言った！`
       }],
     } // リプライするデータを作成
+    // console.log(Deno.env.get("LINE_TOKEN"))
     replies.push(fetch("https://api.line.me/v2/bot/message/reply", {
       method: "POST",
       headers: {
